dd3a176b8ad3c73c1b050d0f9eb8cbe208e8e305
SelrahcD <c.desneuf@gmail.com>
2024-05-10 14:35:03 +0200

    Use layout and view system for form submitted view

--- index.js
+++ index.js
@@ -1,117 +1,18 @@
 // index.js
 const fs = require('fs');
 const express = require('express');
+const { engine } = require('express-handlebars');
 const app = express();
 const port = 3000;
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 
 const filePath = './files/receipt.txt';
 // tag::confirmation[]
 app.get('/', (req, res) => {
-    const htmlContent = `
-    <!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <title>Form submitted</title>
-        <style>
-        .spinner {
-            display: none;
-            border: 4px solid rgba(0, 0, 0, 0.1);
-            width: 16px;
-            height: 16px;
-            border-radius: 50%;
-            border-left-color: #09f;
-            animation: spin 1s linear infinite;
-        }
-
-        @keyframes spin {
-            0% {
-                transform: rotate(0deg);
-            }
-            100% {
-                transform: rotate(360deg);
-            }
-        }
-
-        .hidden {
-            display: none;
-        }
-    </style>
-      </head>
-      <body>
-        <h1>Your form was submitted !</h1>
-        <p>Thank you for your information.</p>
-        <a href="/receipt" class="download">Get your receipt<div class="spinner"></div></a>
-      </body>
-      <script>
-      
-      const downloadLinks = document.getElementsByClassName('download');
-      
-      for(let downloadLink of downloadLinks) {
-          downloadLink.addEventListener('click', function(event) {
-              event.preventDefault();
-              
-              const spinner = event.currentTarget.querySelector('.spinner');
-              spinner.style.display = 'inline-block';
-              
-              fetchWithRetryAfter(() => fetchDownloadAttachment(fetch(this.getAttribute('href'))), 10)
-            .then(() => {
-                spinner.style.display = 'none'
-            })
-            .catch(error => console.error(error))
-          })
-      }
-
-      async function fetchWithRetryAfter(fetch, maxRetries = 5) {
-        let attempt = 0;
-        let response;
-    
-        while (attempt < maxRetries) {
-            attempt++;
-            
-            response = await fetch();
-            
-            if(response.headers.get('Retry-After')) {
-              let delayMs = response.headers.get('Retry-After') * 1000;
-              await new Promise(resolve => setTimeout(resolve, delayMs));
-              continue;
-            }
-            
-            return response;
-        }
-        
-         return response;
-    }
-    
-        // Function to perform a GET request to the current page
-       async function fetchDownloadAttachment(fetch) {
-          const response = await fetch;
-          
-          const contentDisposition = response.headers.get('Content-Disposition');
-          
-          if(contentDisposition && contentDisposition.includes('attachment')) {
-            // Extract filename (if any) from Content-Disposition
-            const filenameMatch = contentDisposition.match(/filename="?(.+)"?/);
-            const filename = filenameMatch ? filenameMatch[1] : 'default-filename';
-
-            // Process the response as a Blob and initiate the download
-            const data = await response.blob();
-            const downloadUrl = window.URL.createObjectURL(data);
-            const a = document.createElement('a');
-            a.href = downloadUrl;
-            a.download = filename;
-            document.body.appendChild(a);
-            a.click();
-            a.remove();
-            window.URL.revokeObjectURL(downloadUrl);
-          }
-          
-          return response;
-      }
-      
-      </script>
-    </html>
-  `;
-    res.send(htmlContent);
+    res.render('formsubmitted', { title: 'Form submitted!' });
 })
 // end::confirmation[]
 
